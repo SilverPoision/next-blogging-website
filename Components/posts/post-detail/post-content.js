61aa23715b21945cd62ce13f887cc0ba484f86aa
Silverpoision <piyush1671@gmail.com>
2022-01-28 11:06:13 +0530

    added a test

--- Components/posts/post-detail/post-content.js
+++ Components/posts/post-detail/post-content.js
@@ -3,8 +3,13 @@ import classes from "./post-content.module.css";
 
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 const PostContent = (props) => {
   const { slug } = props;
